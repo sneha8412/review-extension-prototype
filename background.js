0b5bc5ce2e1e2b50613c02941a063084d8095cc3
v-snagarwal <v-sangarwal@microsoft.com>
2021-12-02 14:21:02 -0800

    deleted unused code. added 32 icon in manifest

--- background.js
+++ background.js
@@ -9,14 +9,5 @@ chrome.runtime.onMessage.addListener( data => {
 					iconUrl: './assets/icons/pika-128.png',
 				}
 			);
-			// const notification = new Notification(
-			// 	`${data.message}`, 
-			// 	{ 
-			// 		body: `${data.message}`, 
-			// 		tag: "Toast Notification From Service worker", 
-			// 		icon: "./assets/icons/Pikachu-128.png", 
-			// 		image: "./assets/icons/pikachu-image.jpg", 
-			// 		requireInteraction: false
-			// 	});
 	}
   });
